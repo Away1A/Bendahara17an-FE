6fc0b3141260064629feda64e4aa039be4c97dd5
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2025-07-14 20:59:29 +0700

    testing

--- src/pages/Laporan.jsx
+++ src/pages/Laporan.jsx
@@ -27,11 +27,11 @@ export default function Laporan() {
   };
 
   const downloadExcel = () => {
-    const url = `https://bendahara17an-be.vercel.app/api/laporan-pdf/download-excel?start_date=${startDate}&end_date=${endDate}`;
+    const url = `https://c2c3a710c7ce.ngrok-free.app/api/laporan-pdf/download-excel?start_date=${startDate}&end_date=${endDate}`;
     window.open(url, "_blank");
   };
   const downloadPdf = () => {
-    const url = `https://bendahara17an-be.vercel.app/api/laporan-pdf?start_date=${startDate}&end_date=${endDate}`;
+    const url = `https://c2c3a710c7ce.ngrok-free.app/api/laporan-pdf?start_date=${startDate}&end_date=${endDate}`;
     window.open(url, "_blank");
   };
 
