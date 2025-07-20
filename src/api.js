7f27708f0a3f9c0361a59a56d01f6f650849887b
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2025-07-20 10:30:35 +0700

    up

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://68dbc27df2a8.ngrok-free.app/api",
+  baseURL: "https://b6f5cbf6f3a5.ngrok-free.app/api",
   headers: {
     "ngrok-skip-browser-warning": "true",
   }, // Ganti jika backend kamu berbeda
--- src/pages/Laporan.jsx
+++ src/pages/Laporan.jsx
@@ -27,11 +27,11 @@ export default function Laporan() {
   };
 
   const downloadExcel = () => {
-    const url = `https://68dbc27df2a8.ngrok-free.app/api/laporan-pdf/download-excel?start_date=${startDate}&end_date=${endDate}`;
+    const url = `https://b6f5cbf6f3a5.ngrok-free.app/api/laporan-pdf/download-excel?start_date=${startDate}&end_date=${endDate}`;
     window.open(url, "_blank");
   };
   const downloadPdf = () => {
-    const url = `https://68dbc27df2a8.ngrok-free.app/api/laporan-pdf?start_date=${startDate}&end_date=${endDate}`;
+    const url = `https://b6f5cbf6f3a5.ngrok-free.app/api/laporan-pdf?start_date=${startDate}&end_date=${endDate}`;
     window.open(url, "_blank");
   };
 
