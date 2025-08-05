dd3d925f1d2ebf9fcead5e604980ee516579b3e5
M Nabil Haritshah Away <82019717+Away1A@users.noreply.github.com>
2025-08-05 11:49:56 +0700

    test 123

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://26eb6ec9aae7.ngrok-free.app/api",
+  baseURL: "https://31416a5128a1.ngrok-free.app/api",
   headers: {
     "ngrok-skip-browser-warning": "true",
   }, // Ganti jika backend kamu berbeda
